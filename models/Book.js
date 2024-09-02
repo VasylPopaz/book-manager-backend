5fc5f94f5a89aa66842598da8dc79174a525c28e
VasylPopaz <pafos1997@gmail.com>
2024-09-02 12:18:51 +0200

    add Book model

---
+++ models/Book.js
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+import { isbnRegexp } from "../constants/index.js";
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+    },
+    author: {
+      type: String,
+      required: [true, "Author is required."],
+    },
+    isbn: {
+      type: String,
+      match: isbnRegexp,
+      unique: true,
+      required: [true, "isbn is required."],
+    },
+    isBorrowed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+
+export const Book = model("book", bookSchema);

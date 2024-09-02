1e25dca7975d8cc83481da61f631a388152adc01
VasylPopaz <pafos1997@gmail.com>
2024-09-02 14:58:22 +0200

    upd createBookSchema

--- schemas/booksSchemas.js
+++ schemas/booksSchemas.js
@@ -5,7 +5,12 @@ import { isbnRegexp } from "../constants/index.js";
 export const createBookSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
-  isbn: Joi.string().required().pattern(isbnRegexp).length(17),
+  isbn: Joi.string().required().pattern(isbnRegexp).length(17).messages({
+    "string.pattern.base": "The ISBN format is incorrect.",
+    "string.length": "The ISBN must be exactly 17 characters long.",
+    "string.base": "ISBN must be a string.",
+    "string.empty": "ISBN is required.",
+  }),
   isBorrowed: Joi.boolean(),
 });
 export const updateStatusBookSchema = Joi.object({

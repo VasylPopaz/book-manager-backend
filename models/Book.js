83ec689da2768879fe9399ac583a49e1b533fd21
VasylPopaz <pafos1997@gmail.com>
2024-09-02 14:58:48 +0200

    add isbnValidator

--- models/Book.js
+++ models/Book.js
@@ -2,6 +2,25 @@ import { model, Schema } from "mongoose";
 
 import { isbnRegexp } from "../constants/index.js";
 
+const isbnValidator = {
+  validator: function (value) {
+    if (value.length !== 17) {
+      return false;
+    }
+
+    return isbnRegexp.test(value);
+  },
+  message: function (props) {
+    if (props.value.length !== 17) {
+      return "The ISBN must be exactly 17 characters long.";
+    }
+    if (!isbnRegexp.test(props.value)) {
+      return "The ISBN format is incorrect.";
+    }
+    return "Invalid ISBN.";
+  },
+};
+
 const bookSchema = new Schema(
   {
     title: {
@@ -14,7 +33,7 @@ const bookSchema = new Schema(
     },
     isbn: {
       type: String,
-      match: isbnRegexp,
+      validate: isbnValidator,
       unique: true,
       required: [true, "isbn is required."],
     },
